69d237affc1027b965dce929e0fb8434312de08f
shigobu <shigobu@shigobu.com>
2018-06-26 13:02:06 +0900

    アイコン追加 プログレスバー追加

--- バックアップはできますWPF/MainWindow.xaml.cs
+++ バックアップはできますWPF/MainWindow.xaml.cs
@@ -7,6 +7,7 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+using System.Windows.Forms;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
@@ -23,6 +24,38 @@ namespace バックアップはできますWPF
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            //FolderBrowserDialogクラスのインスタンスを作成
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+
+            //上部に表示する説明テキストを指定する
+            fbd.Description = "フォルダを指定してください。";
+            //ルートフォルダを指定する
+            fbd.RootFolder = Environment.SpecialFolder.MyComputer;
+            //最初に選択するフォルダを指定する
+            //RootFolder以下にあるフォルダである必要がある
+            fbd.SelectedPath = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+            //ユーザーが新しいフォルダを作成できるようにする
+            //デフォルトでTrue
+            fbd.ShowNewFolderButton = false;
+
+            //ダイアログを表示する
+            if (fbd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                //選択されたフォルダを表示する
+                textBox1.Text = fbd.SelectedPath;
+            }
+            fbd.Dispose();
+        }
+
+        private void DoButton_Click(object sender, RoutedEventArgs e)
+        {
+            バックアップはできますWPF.ProgressBar progressBar = new バックアップはできますWPF.ProgressBar();
+            progressBar.Owner = this;
+            progressBar.Show();
         }
     }
 }
