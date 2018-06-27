98aa288cdc73076aef35106633a1756458c2c468
shigobu <shigobu@shigobu.com>
2018-06-27 11:10:23 +0900

    バツボタン無効

--- バックアップはできますWPF/ProgressBar.xaml.cs
+++ バックアップはできますWPF/ProgressBar.xaml.cs
@@ -4,16 +4,9 @@ using System.ComponentModel;
 using System.Globalization;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Runtime.InteropServices;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+using System.Windows.Interop;
 
 namespace バックアップはできますWPF
 {
@@ -25,6 +18,27 @@ namespace バックアップはできますWPF
         MainWindow SelectForm;
         ResultForm ResultForm;
         BackgroundWorker backgroundWorker1;
+
+        #region "最大化・最小化・閉じるボタンの非表示設定"
+        [DllImport("user32.dll")]
+        private static extern int GetWindowLong(IntPtr hWnd, int nIndex);
+
+        [DllImport("user32.dll")]
+        private static extern int SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
+
+        const int GWL_STYLE = -16;
+        const int WS_SYSMENU = 0x80000;
+
+        protected override void OnSourceInitialized(EventArgs e)
+        {
+            base.OnSourceInitialized(e);
+            IntPtr handle = new WindowInteropHelper(this).Handle;
+            int style = GetWindowLong(handle, GWL_STYLE);
+            style = style & (~WS_SYSMENU);
+            SetWindowLong(handle, GWL_STYLE, style);
+        }
+        #endregion
+
         public ProgressBar(MainWindow mainWindow, ResultForm resultForm)
         {
             InitializeComponent();
